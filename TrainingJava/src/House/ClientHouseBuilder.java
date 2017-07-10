598bccd9215572559509e6805a3a673abeb97273
amlietti87 <amlietti@gmail.com>
2017-07-10 11:03:13 -0300

    Made the changes commented.

--- TrainingJava/src/House/ClientHouseBuilder.java
+++ TrainingJava/src/House/ClientHouseBuilder.java
@@ -1,16 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package House;
 
 import java.util.Scanner;
 
-/**
- *
- * @author andreslietti
- */
+
+ //@author andreslietti
+ 
 
 // In this class the house is created requesting data from a client. 
 public class ClientHouseBuilder implements HouseBuilder {
@@ -24,46 +19,41 @@ public class ClientHouseBuilder implements HouseBuilder {
     @Override
     public void buildStructure() {
         System.out.println("Choose a structure: ");
-        String choice;
         Scanner scan = new Scanner (System.in); 
-        choice = scan.nextLine (); 
+        String choice = scan.nextLine (); 
         house.setStructure(choice);
     }
 
     @Override
     public void buildRoofTyoe() {
         System.out.println("Choose type of roof: ");
-        String choice;
-        Scanner scan = new Scanner (System.in);
-       choice = scan.nextLine (); 
+        Scanner scan = new Scanner (System.in); 
+        String choice = scan.nextLine (); 
         house.setRoofType(choice);
     }
 
     @Override
     public void NumberOfFloors() {
         System.out.println("Choose Number of Floors: ");
-        int choice;
         Scanner scan = new Scanner (System.in); 
-        choice = scan.nextInt(); 
+        int choice = scan.nextInt(); 
         house.setNumberOfFloors(choice);
     }
 
     @Override
     public void buildOutdoorType() {
         System.out.println("Choose a type of Outdoors: ");
-        String choice;
         Scanner scan = new Scanner (System.in); 
-        choice = scan.nextLine (); 
+        String choice = scan.nextLine (); 
         house.setOutdoorType(choice);
     }
 
     @Override
     public void FurnishedHouse() {
         System.out.println("Do you want furnished? ");
-        String choice;
         Scanner scan = new Scanner (System.in); 
-        choice = scan.nextLine (); 
-        if (choice.contains("yes")) {
+        String choice = scan.nextLine (); 
+        if ("yes".equals(choice)) {
             house.setFurnished("Furnished");
         }
             house.setFurnished("Not Furnished");
@@ -71,7 +61,7 @@ public class ClientHouseBuilder implements HouseBuilder {
 
     @Override
     public House getHouse() {
-        return house;
+        return this.house;
     }
     
 }
